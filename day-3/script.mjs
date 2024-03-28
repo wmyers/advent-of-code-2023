b578f30f543e83016ae560dfa16009ca67bed5fd
William Myers <wmyers@Williams-MacBook-Pro.local>
2024-03-28 11:05:30 +0000

    day-3 pt2

--- day-3/script.mjs
+++ day-3/script.mjs
@@ -1,3 +1,4 @@
+import 'core-js/actual/set/index.js';
 import { readFileSync } from "node:fs";
 
 const filename = process.argv[2];
@@ -7,50 +8,67 @@ const fileInputArray = fileInput.split('\n').filter(line => line.length > 0);
 // create two dimensional array
 const twoDArr = fileInputArray.map(line => Array.from(line));
 const partNumbers = [];
+const gearRecord = new Map();
 
-const checkPartNumber = (rowIndex, colIndex, rowLength) => {
+const processNeighbours = (neighboursPositions, tdArr) => {
+  const adjacentGearPositions = new Set();
+  const neighbourSymbols = neighboursPositions.map(([row, col]) => {
+    const symbol = tdArr[row][col];
+    if(symbol === '*'){
+      const key = `[${row}, ${col}]`;
+      adjacentGearPositions.add(key);
+    }
+    return symbol;
+  });
+
+  // check if the number is adjacent any symbol (not '.' or a digit)
+  const isPartNumber = !!(neighbourSymbols.find(n => (n !== '.' && !/\d/.test(n))));
+  return [isPartNumber, adjacentGearPositions];
+}
+
+const checkPartNumber = (rowIndex, colIndex, rowLength, tdArr) => {
   const neighbours = [];
-  const arr = twoDArr;
   // add above row neighbours
   if (rowIndex > 0){
     // above-middle
-    neighbours.push(arr[rowIndex-1][colIndex]);
+    neighbours.push([rowIndex-1, colIndex]);
     if (colIndex > 0){
       // above-left
-      neighbours.push(arr[rowIndex-1][colIndex-1]);
+      neighbours.push([rowIndex-1, colIndex-1]);
     }
     if (colIndex < rowLength-1){
       // above-right
-      neighbours.push(arr[rowIndex-1][colIndex+1]);
+      neighbours.push([rowIndex-1, colIndex+1]);
     }
   }
   // add below neighbours
-  if (rowIndex < arr.length-1){
+  if (rowIndex < tdArr.length-1){
     // below-middle
-    neighbours.push(arr[rowIndex+1][colIndex]);
+    neighbours.push([rowIndex+1, colIndex]);
     if (colIndex > 0){
       // below-left
-      neighbours.push(arr[rowIndex+1][colIndex-1]);
+      neighbours.push([rowIndex+1, colIndex-1]);
     }
     if (colIndex < rowLength-1){
       // below-right
-      neighbours.push(arr[rowIndex+1][colIndex+1]);
+      neighbours.push([rowIndex+1, colIndex+1]);
     }
   }
   // add left and right neighbours 
   if (colIndex > 0){
     // left
-    neighbours.push(arr[rowIndex][colIndex-1]);
+    neighbours.push([rowIndex, colIndex-1]);
   }
   if (colIndex < rowLength-1){
     // right
-    neighbours.push(arr[rowIndex][colIndex+1]);
+    neighbours.push([rowIndex, colIndex+1]);
   }
-  return !!(neighbours.find(n => (n !== '.' && !/\d/.test(n))));
+  return processNeighbours(neighbours, tdArr);
 }
 
 twoDArr.forEach((row, rowIndex) => {
   let numberInRow = '';
+  let numberInRowGearRecord = new Set();
   let isPartNumber = false;
   row.forEach((val, colIndex) => {
     // is that row/col entry a digit?
@@ -59,11 +77,24 @@ twoDArr.forEach((row, rowIndex) => {
       numberInRow = numberInRow.concat(val);
       // check for a partNumber
       if(!isPartNumber){
-        isPartNumber = checkPartNumber(rowIndex, colIndex, row.length)
+        const [isPartNumberCalculated, adjacentGearPositions] = checkPartNumber(rowIndex, colIndex, row.length, twoDArr);
+        isPartNumber = isPartNumberCalculated;
+        
+        if(adjacentGearPositions.size > 0){
+          // update the current gear record for the current numberInRow
+          numberInRowGearRecord = numberInRowGearRecord.union(adjacentGearPositions);
+        }
       }
       // check if the digit is in the last col in the row
       if(colIndex === row.length-1 && isPartNumber){
         partNumbers.push(Number(numberInRow));
+
+        // also update the main gearRecord map for this numberInRow
+        if(numberInRowGearRecord.size > 0){
+          numberInRowGearRecord.forEach(val => {
+            gearRecord.set(val, (gearRecord.get(val) || []).concat([Number(numberInRow)]))
+          })
+        }
       }
     } else {
       // check when you have moved from a digit val to a non-digit val if there is a partNumber now ready to be recorded. 
@@ -71,16 +102,27 @@ twoDArr.forEach((row, rowIndex) => {
       if (numberInRow !== '' && isPartNumber){
         // add any part number to the array
         partNumbers.push(Number(numberInRow));
+
+        // also update the main gearRecord map for this numberInRow
+        if(numberInRowGearRecord.size > 0){
+          numberInRowGearRecord.forEach(val => {
+            gearRecord.set(val, (gearRecord.get(val) || []).concat([Number(numberInRow)]))
+          })
+        }
       }
       // reset flags
       numberInRow = '';
+      numberInRowGearRecord = new Set();
       isPartNumber = false;
     }
   })
 })
 
-const sumPartNumbers = partNumbers.reduce((curr, next) => curr + next, 0 )
+const sumPartNumbers = partNumbers.reduce((curr, next) => curr + next, 0 );
+const sumGearRatios = Array.from(gearRecord.values()).filter(arr => arr.length === 2).reduce((c, n) => c + (n[0] * n[1]), 0);
 
 // console.log('>> twoDArr', twoDArr);
 // console.log('>> partNumbers', partNumbers, partNumbers.length);
 console.log('>> sumPartNumbers',sumPartNumbers);
+// console.log('>> gearRecord', gearRecord);
+console.log('>> sumGearRatios', sumGearRatios);
