a7f5e1375e5d4895b16bd26492fe7451cb4d4cd0
Rick Behl <rickbehl@gmail.com>
2024-03-28 10:29:01 +0000

    Initial working code for day 4

---
+++ day-4/script.mjs
@@ -0,0 +1,38 @@
+// Advent of Code - Day 04
+import { readFileSync } from "node:fs";
+
+// Read the file
+const parseFile = filename => {
+  const fileInput = readFileSync(filename).toString();
+  const fileInputArray = fileInput.split('\n').filter(line => line.length > 0);
+  return fileInputArray;
+}
+
+// Check entries and return final score 
+const checkEntries = fileInputArray => {
+  let total = 0;
+
+  fileInputArray.map(line => {
+    const [winningNumbersString, entryNumbersString] = line.split(': ')[1].split('|');    // Remove card number and split winning numbers and entries
+    const winningNumbers = winningNumbersString.match(/\d+/g);      // Convert to an array of winning numbers
+    const entryNumbers = entryNumbersString.match(/\d+/g);          // Convert to an array of entry numbers
+  
+    // Check each of the entry numbers against the winning numbers producing a 1 if matched, then sum these 1's
+    const numWinningEntries = entryNumbers.map(n => winningNumbers.includes(n) ? 1 : 0).reduce((a, b) => a + b);
+
+    // Total is incremented by 2^(Matching entries - 1)
+    total += numWinningEntries > 0 ? Math.pow(2, numWinningEntries - 1) : 0;
+  });  
+
+  return total;
+}
+
+// Main
+const main = () => {
+  const filename = process.argv[2];
+  const fileInputArray = parseFile(filename);
+  const result = checkEntries(fileInputArray);
+  return result;
+}
+
+console.log(main());
