898f47ab86a250cc4f766b59163967d91826d577
Rick Behl <rickbehl@gmail.com>
2024-04-14 13:18:07 +0100

    Completed Day 9 Puzzle 2

--- day-9/script-rb.mjs
+++ day-9/script-rb.mjs
@@ -2,16 +2,25 @@ import fs, { read } from 'node:fs';
 
 const calculateNextNumberPuzzleOne = readings => {
     let nextNumber = 0;
-    let allReadings = calculateAllStepsPuzzleOne(readings);
-
-    for (let j = 0; j < allReadings.length; j++) {
-        nextNumber += allReadings[j].at(-1);
-    }
-
+    let allReadings = calculateAllSteps(readings);
+    nextNumber = allReadings.map(reading => reading.at(-1)).reduce((a, b) => a + b)
     return nextNumber;
 }
 
-const calculateAllStepsPuzzleOne = readings => {
+const calculateNextNumberPuzzleTwo = readings => {
+    let nextNumber = 0;
+    let allReadings = calculateAllSteps(readings);
+    allReadings[allReadings.length - 1] = [0].concat(allReadings.at(-1));
+
+    for (let j = allReadings.length - 2; j >= 0; j--) {
+        allReadings[j] = [allReadings[j][0] - allReadings[j + 1][0]].concat(allReadings[j]);
+    }
+
+    nextNumber = allReadings[0][0];
+    return nextNumber;    
+}
+
+const calculateAllSteps = readings => {
     let nextReadings = [];
     
     for (let i = 1; i < readings.at(-1).length; i++) {
@@ -21,7 +30,7 @@ const calculateAllStepsPuzzleOne = readings => {
     readings.push(nextReadings);
 
     if (nextReadings.filter(number => number !== 0).length > 0) {
-        readings = calculateAllStepsPuzzleOne(readings);
+        readings = calculateAllSteps(readings);
     } 
 
     return readings;
@@ -30,13 +39,19 @@ const calculateAllStepsPuzzleOne = readings => {
 const main = () => {
     let puzzleOneResult = 0;
     let puzzleTwoResult = 0;
+    let thisRowAnswer = 0;
+
     const reportData = fs.readFileSync(process.argv[2]).toString().split('\n');
 
     reportData.map(row => {
-        let thisRowAnswer = calculateNextNumberPuzzleOne([row.split(' ').map(Number)]);
+        thisRowAnswer = calculateNextNumberPuzzleOne([row.split(' ').map(Number)]);
         puzzleOneResult += thisRowAnswer;
     });
     
+    reportData.map(row => {
+        thisRowAnswer = calculateNextNumberPuzzleTwo([row.split(' ').map(Number)]);
+        puzzleTwoResult += thisRowAnswer;
+    });
     
     console.log(`Puzzle One: ${puzzleOneResult}, Puzzle Two: ${puzzleTwoResult}`);
 }
